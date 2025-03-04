605893c5b8fe3226a552d9d8f3c3e62b35d1137a
duongdz123 <doanvanduong1472021@gmail.com>
2025-03-04 20:42:03 +0700

    Tạo api key và check auth

---
+++ src/auth/authUntils.js
@@ -0,0 +1,29 @@
+const JWT = require('jsonwebtoken')
+const createToKenPair = async (payload, publicKey, privateKey) => {
+    try {
+
+        const accessToken = await JWT.sign(payload, publicKey, {
+
+            expiresIn: '2 days'
+        })
+        const represhToken = await JWT.sign(payload, privateKey, {
+
+            expiresIn: '7 days'
+        })
+
+        JWT.verify(accessToken, publicKey, (err, decode) => {
+            if (err) {
+                console.error('err verify::', err)
+            } else {
+                console.log('decode verify::', decode);
+
+            }
+        })
+        return { accessToken, represhToken }
+    } catch (error) {
+
+    }
+}
+module.exports = {
+    createToKenPair
+}
