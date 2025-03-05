7a3b61d0306d3e82a2ca1d6e235993efd3f48df7
tranvu <trandinhvu1805@gmail.com>
2025-03-05 20:31:09 +0700

    merge code and delete files

--- src/auth/authUtils.js
+++ src/auth/authUtils.js
@@ -1,25 +1,23 @@
-"use strict";
-
 const JWT = require("jsonwebtoken");
-
-const createTokenPair = async (payload, publicKey, privateKey) => {
+const createToKenPair = async (payload, publicKey, privateKey) => {
   try {
     const accessToken = await JWT.sign(payload, publicKey, {
       expiresIn: "2 days",
     });
-    const refreshToken = await JWT.sign(payload, privateKey, {
+    const represhToken = await JWT.sign(payload, privateKey, {
       expiresIn: "7 days",
     });
 
-    JWT.verify(accessToken, publicKey, (err, decoded) => {
+    JWT.verify(accessToken, publicKey, (err, decode) => {
       if (err) {
-        console.error(`error verify:: `, err);
+        console.error("err verify::", err);
       } else {
-        console.log(`decoded:: `, decoded);
+        console.log("decode verify::", decode);
       }
     });
-    return { accessToken, refreshToken };
+    return { accessToken, represhToken };
   } catch (error) {}
 };
-
-module.exports = { createTokenPair };
+module.exports = {
+  createToKenPair,
+};
