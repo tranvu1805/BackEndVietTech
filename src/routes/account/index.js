66e2edeb08feb29e56253f285aad5a462bb342aa
duongdz123 <doanvanduong1472021@gmail.com>
2025-03-10 22:47:21 +0700

    Sửa lại logout cần chuyền vào represtoken, getAcount kèm get role luôn

--- src/routes/account/index.js
+++ src/routes/account/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const AccountController = require("../../controllers/account.controller");
 const { apiKey, permissions } = require("../../auth/checkAuth");
 
-router.get("/:id", AccountController.getAccount);
+router.get("/:id", AccountController.getAccount); // Lấy thông tin tài khoản và quyền
 router.put("/update/:id", AccountController.updateAccount);
 router.put("/status/:id", AccountController.updateAccountStatus);
 
